2f8f6bc81914a07e8b0451df883661affd056aca
Bradley Holbrook <bradley@praetorianstudio.com>
2020-09-17 17:46:33 -0700

    Added type definitions for parts of data required

---
+++ src/house/interface.ts
@@ -0,0 +1,25 @@
+export interface IWallCalculatorResponse {
+    posts: number;
+
+    studs: number;
+
+    plates: number;
+}
+
+export interface IHouseProperties {
+    name: string;
+
+    width: number;
+
+    length: number;
+}
+
+export interface IHouseMaterials {
+    posts: number;
+
+    studs: number;
+
+    plates: number;
+}
+
+export interface IHouse extends IHouseProperties, IHouseMaterials {}
